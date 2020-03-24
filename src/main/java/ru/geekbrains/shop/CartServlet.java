69563dc193082ddd699b255c57619007aa886c12
maxkru <max_kru@hotmail.com>
2020-03-24 20:02:55 +0300

    Servlets added: main, cart, catalog, order, product

---
+++ src/main/java/ru/geekbrains/shop/CartServlet.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.shop;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CartServlet")
+public class CartServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter respWriter = resp.getWriter();
+        respWriter.print("<h1>Корзина</h1>");
+    }
+}
---
+++ src/main/java/ru/geekbrains/shop/MainServlet.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.shop;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MainServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter respWriter = resp.getWriter();
+        respWriter.print("<h1>Главная страница</h1>");
+    }
+}
