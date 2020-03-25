d25eaf8f52f0611403a57d2ed59c5b92e9402f79
maxkru <max_kru@hotmail.com>
2020-03-25 20:46:52 +0300

    NavigationBarServlet added

--- src/main/java/ru/geekbrains/shop/CatalogServlet.java
+++ src/main/java/ru/geekbrains/shop/CatalogServlet.java
@@ -13,10 +13,8 @@ public class CatalogServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter respWriter = resp.getWriter();
         respWriter.print("<h1>Каталог</h1>");
-        respWriter.printf("<a href=%1$s/main>Главная страница</a> " +
-                "<a href=%1$s/cart>Корзина</a> " +
-                "<a href=%1$s/order>Оформление заказа</a> " +
-                "<a href=%1$s/product>Описание товара</a> " +
-                "Каталог", getServletContext().getContextPath());
+        req.setAttribute("page", "catalog");
+        req.setAttribute("contextPath", getServletContext().getContextPath());
+        getServletContext().getRequestDispatcher("/navbar").include(req, resp);
     }
 }
--- src/main/java/ru/geekbrains/shop/ProductServlet.java
+++ src/main/java/ru/geekbrains/shop/ProductServlet.java
@@ -13,10 +13,8 @@ public class ProductServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter respWriter = resp.getWriter();
         respWriter.print("<h1>Описание товара</h1>");
-        respWriter.printf("<a href=%1$s/main>Главная страница</a> " +
-                "<a href=%1$s/cart>Корзина</a> " +
-                "<a href=%1$s/order>Оформление заказа</a> " +
-                "Описание товара " +
-                "<a href=%1$s/catalog>Каталог</a>", getServletContext().getContextPath());
+        req.setAttribute("page", "product");
+        req.setAttribute("contextPath", getServletContext().getContextPath());
+        getServletContext().getRequestDispatcher("/navbar").include(req, resp);
     }
 }
