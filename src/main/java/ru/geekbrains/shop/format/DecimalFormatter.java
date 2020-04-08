85748b5c833556132e1b96944a0eb310cd37c04f
maxkru <max_kru@hotmail.com>
2020-04-08 14:24:11 +0300

    Price formatting added

---
+++ src/main/java/ru/geekbrains/shop/format/DecimalFormatter.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.shop.format;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+@Named
+@ApplicationScoped
+public class DecimalFormatter {
+    private static DecimalFormat format = new DecimalFormat("# ###.00");
+
+    public String format(BigDecimal n) {
+        return format.format(n);
+    }
+}
