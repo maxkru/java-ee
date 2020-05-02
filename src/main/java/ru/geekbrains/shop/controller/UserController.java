982d429c27e9c72247109eda5689f16cc8cf312a
maxkru <max_kru@hotmail.com>
2020-05-02 10:37:15 +0300

    Login and user modification pages added

--- src/main/java/ru/geekbrains/shop/controller/UserController.java
+++ src/main/java/ru/geekbrains/shop/controller/UserController.java
@@ -10,6 +10,7 @@ import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Named
@@ -44,5 +45,30 @@ public class UserController implements Serializable {
         return users;
     }
 
+    public List<UserRoleRepr> getAllRoles() {
+        return roles;
+    }
 
+    public List<UserRoleRepr> getUserRoles() {
+        return new ArrayList<>();
+    }
+
+    public String createUser() {
+        userRepr = new UserRepr();
+        return "/admin/user.xhtml?faces-redirect=true";
+    }
+
+    public String saveUser() {
+        userService.updateUser(userRepr);
+        return "/admin/users.xhtml?faces-redirect=true";
+    }
+
+    public String editUser(UserRepr userRepr) {
+        this.userRepr = userRepr;
+        return "/admin/user.xhtml?faces-redirect=true";
+    }
+
+    public void deleteUser(UserRepr userRepr) {
+        userService.removeUser(userRepr.getId());
+    }
 }
